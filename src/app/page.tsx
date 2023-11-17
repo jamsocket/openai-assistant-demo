cb66ba2ccf122c77f615e2ffcfb9a3152c3ebae8
Taylor Baldwin <taylorbaldwin@gmail.com>
2023-11-17 10:50:29 -0500

    updates to make this its own demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import 'server-only';
 import HomeContainer from '../components/Home'
 import { init } from '@jamsocket/javascript/server'
 
-const WHITEBOARD_NAME = 'whiteboard-demo/default'
+const WHITEBOARD_NAME = 'openai-assistant-demo/default'
+const OPENAI_API_KEY = '[YOUR OPENAI_API_KEY HERE]'
 
 const spawnBackend = init({
-  account: 'felicia',
-  service: 'whiteboard-demo',
+  account: '[YOUR JAMSOCKET ACCOUNT NAME HERE]',
+  service: 'openai-assistant-demo',
   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
   // We'll leave this blank for now, since we don't need it when developing with the dev CLI
   token: '',
@@ -14,6 +15,9 @@ const spawnBackend = init({
 })
 
 export default async function Page() {
-  const spawnResult = await spawnBackend({ lock: WHITEBOARD_NAME })
+  const spawnResult = await spawnBackend({
+    lock: WHITEBOARD_NAME,
+    env: { OPENAI_API_KEY }
+  })
   return <HomeContainer spawnResult={spawnResult} />
 }
