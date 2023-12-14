94e0a1ab7e09cbf8ba53ae1570340814ca6e547c
Felicia <34881756+feliciachang@users.noreply.github.com>
2023-12-14 13:21:58 -0800

    keeping message while run is processing (#7)

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSend } from '@jamsocket/javascript/react'
 
 interface ChatProps {
@@ -8,22 +8,31 @@ interface ChatProps {
 export default function Chat(props: ChatProps) {
   const { canAcceptMessages } = props
   const [message, setMessage] = useState('')
+  const [placeholder, setPlaceholder] = useState('Write a message...')
   const sendEvent = useSend()
+
+  useEffect(() => {
+    if (canAcceptMessages) {
+      setPlaceholder('Write a message...')
+    }
+  }, [canAcceptMessages])
+
   return (
     <div className="my-4 w-1/2">
       <form
         onSubmit={async (e) => {
           e.preventDefault()
           sendEvent('handle-user-prompt', message)
+          setPlaceholder(message)
           setMessage('')
         }}
       >
         <input
           type="text"
-          placeholder="Write a message..."
+          placeholder={placeholder}
           className={`${
             canAcceptMessages ? 'cursor-default' : 'cursor-not-allowed'
-          } w-full px-4 py-2 bg-gray-600 caret-neutral-200 text-white text-sm rounded-lg`}
+          } w-full px-4 py-2 bg-gray-600 text-white caret-neutral-200 text-sm rounded-lg`}
           value={message}
           disabled={!canAcceptMessages}
           onChange={(e) => {
