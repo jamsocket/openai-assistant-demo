b24811d46f65a9f383b997d816870d713934de3b
Felicia <ffeliciachang@gmail.com>
2024-02-07 15:09:50 -0800

    update library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import HomeContainer from '../components/Home'
 import { init } from '@jamsocket/javascript/server'
 
 const WHITEBOARD_NAME = 'openai-assistant-demo/default'
-const OPENAI_API_KEY = '[YOUR OPENAI_API_KEY HERE]'
+const OPENAI_API_KEY = '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'
 
 const spawnBackend = init({
-  account: '[YOUR JAMSOCKET ACCOUNT NAME]',
+  account: 'felicia',
   service: 'openai-assistant-demo',
   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
   // We'll leave this blank for now, since we don't need it when developing with the dev CLI
