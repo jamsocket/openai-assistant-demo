238e2a0dec1ecba15832610c751ae86c3415f6ce
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-02-15 13:06:41 -0500

    use @jamsocket/javascript v0.2.0 and update link for when we move this repo to the jamsocket org

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import 'server-only'
 import HomeContainer from '../components/Home'
-import { init } from '@jamsocket/javascript/server'
+import Jamsocket from '@jamsocket/javascript/server'
 
 const WHITEBOARD_NAME = 'openai-assistant-demo/default'
 const OPENAI_API_KEY = '[YOUR OPENAI_API_KEY HERE]'
 
-const spawnBackend = init({ dev: true })
+const jamsocket = Jamsocket.init({ dev: true })
 
 export default async function Page() {
-  const spawnResult = await spawnBackend({
+  const spawnResult = await jamsocket.spawn({
     lock: WHITEBOARD_NAME,
     env: { OPENAI_API_KEY },
   })
